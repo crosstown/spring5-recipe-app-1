9d56893faf106ed1555acde024c435adfb75d406
Fernando Simon <royalplanet2009@gmail.com>
2017-11-03 18:13:16 -0400

    Commint new classes from video

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,45 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+/* @author Fernando Simon
+   @date 11/3/2017
+   @time: 5:38 PM
+*/
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
